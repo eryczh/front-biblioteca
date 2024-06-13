a322846a7722ecf665cff9aca7d2d9de37ba2c77
eryczh <ericandrade2810@gmail.com>
2024-06-13 00:44:44 -0300

    imagens funcionando

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,22 +1,21 @@
 import './index.scss';
 import { API_ADDRESS } from '../../api/constant';
 
-export default function Card ({ item }) {
-    //const imgSrc = item.foto ? `${API_ADDRESS}/storage/capa/${item.foto.replace(/\\/g, "/")}` : "assets/images/hp1.jpg";
-
-    const imgSrc = `${API_ADDRESS}/${item.foto}`;
+export default function Card({ item }) {
+    // Verifique se item e item.foto existem antes de tentar usar replace
+    const imgSrc = item && item.foto ? `${API_ADDRESS}/${item.foto.replace(/\\/g, "/")}` : "assets/images/hp1.jpg";
 
     console.log("Image URL:", imgSrc);
+
     return (
         <div className='card'>
             <div className='content'>
                 <img src={imgSrc} alt='capa Livro' />
-                
                 <div>
-                    <h1 className={`${item.titulo}`}>{item.titulo ?? 'titulo livro'}</h1>
-                    <span>{item.genero ?? 'genero'}</span>
+                    <h1 className={`${item?.titulo ?? 'titulo livro'}`}>{item?.titulo ?? 'titulo livro'}</h1>
+                    <span>{item?.genero ?? 'genero'}</span>
                 </div>
             </div>
         </div>
-    )
+    );
 }
